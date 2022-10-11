6aee39f797eb36206cc65693e503adbe589a0aa6
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-10-11 00:21:50 -0500

    8. feat: setting cookie options

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const app = express();
 app.use(cookieParser());
 
 app.get('/setcookie', (req, res) => {
-    res.cookie('my cookie name', 'my cookie');
+    res.cookie('my cookie name', 'my cookie', {
+        // maxAge: 7000,
+        expires: new Date("2022-12-31"), //fecha de expiración
+        httpOnly: true, //indica si solo el server accede o no a la cookie
+        secure: true, //indica si la cookie será utilizada solo con HTTPS
+        sameSite: 'lax' // determina si la cookie será enviada de acuerdo al sitio donde se estableció la misma. Strict: si se originó en el sitio web. Lax: cuando se accede a través de un sitio externo.
+    });
     res.send('algo')
 });
 
