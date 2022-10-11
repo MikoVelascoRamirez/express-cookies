242d9ba87acccf750b6ea681f05c71ae53072877
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-10-10 22:47:14 -0500

    7. feat: setting a cookie

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 
+app.get('/setcookie', (req, res) => {
+    res.cookie('my cookie name', 'my cookie');
+    res.send('algo')
+});
+
 module.exports = app;
